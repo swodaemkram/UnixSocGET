0cfd7e41c3cef98f9e0a31083b338af4a2ad3024
mark <mark@mark-SD331>
2018-11-21 12:35:27 -0500

    UnixSocGET

---
+++ UnixSocGET/src/UnixSocGET.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : UnixSocGET.c
+ Author      : Mark Meadows
+ Version     :
+ Copyright   : copyright 2018
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("SocGET"); /* prints SocGET */
+	return EXIT_SUCCESS;
+}
